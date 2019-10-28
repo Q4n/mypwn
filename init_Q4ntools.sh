6edaba2e0fefdd7d431204b4522d57fa3ad4c6b1
Q4n <1677609544@qq.com>
2019-10-28 22:59:23 +0800

    renew

--- init_Q4ntools.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-cp Q4ntools.py ~/.local/lib/python2.7/site-packages/q4n.py
---
+++ setup.py
@@ -0,0 +1,18 @@
+# -*- coding:utf-8 -*-
+from setuptools import setup, find_packages
+
+setup(
+name = "q4n",
+version = "0.1",
+keywords = ("pwn"),
+description = "my pwn tools",
+license = "MIT Licence",
+
+author = "Q4n",
+author_email = "907659303@qq.com",
+
+packages = find_packages(),
+include_package_data = True,
+platforms = "any",
+install_requires = []
+)
