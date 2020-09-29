43812a615a85e2e66f33a32d6912f59c1f45f5ae
Q4n <1677609544@qq.com>
2020-09-29 16:07:22 +0800

    fix a bug

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
 name = "q4n",
-version = "0.1.7",
+version = "0.1.8",
 keywords = ("pwn"),
 description = "my pwntools",
 license = "MIT Licence",
@@ -15,5 +15,5 @@ author_email = "907659303@qq.com",
 packages = find_packages(),
 include_package_data = True,
 platforms = "any",
-install_requires = ["pwntools","pycrypto","codecs"]
+install_requires = ["pwntools","pycrypto"]
 )
