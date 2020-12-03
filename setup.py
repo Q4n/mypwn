be530f0c64c7bc466939a7984f44693390a67a81
Q4n <1677609544@qq.com>
2020-12-03 17:43:55 +0800

    update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
 name = "q4n",
-version = "0.3.1",
+version = "0.3.2",
 keywords = ("pwn"),
 description = "my pwntools",
 license = "MIT Licence",
@@ -15,5 +15,5 @@ author_email = "907659303@qq.com",
 packages = find_packages(),
 include_package_data = True,
 platforms = "linux",
-install_requires = ["pwntools","pycrypto"]
+install_requires = ["pwntools","pycrypto","requests"]
 )
